e8d5e97d51cd4f79795358c3b880ecc077905d35
harshid9125 <harshidkeraliya@gmail.com>
2023-12-09 22:09:32 +0530

    final update

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -120,7 +120,7 @@ resource "aws_security_group" "alb_security_group" {
     to_port     = 30080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  } 
 
   ingress {
     description = "MySQL from VPC"
